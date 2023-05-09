37df55091217a06382483ca799842dedc36cc789
hollow <571339274@qq.com>
2023-05-09 22:48:05 +0800

    基础搭建

---
+++ src/main/java/com/hollow/CabinetFactoryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hollow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetFactoryManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabinetFactoryManagementSystemApplication.class, args);
+    }
+
+}
